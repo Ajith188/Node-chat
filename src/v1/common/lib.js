b448d7837d6d8138c9df2bc6684a6e787a7e6e42
Ajith188 <102527979+Ajith188@users.noreply.github.com>
2023-09-06 12:52:14 +0530

    Update lib.js

--- src/v1/common/lib.js
+++ src/v1/common/lib.js
@@ -16,10 +16,13 @@ exports.verifyToken=async function(req, res, next) {
     const bearerToken = token.slice(7);
     try {
       const decoded = jwt.verify(bearerToken, config.Jwt_Secret_Key)
-      req.user = decoded
-    //   if (req.body.account!== decoded.account) {
-    //       return res.status(403).json({ message: "Access denied" });
-    //   }
+      // console.log(decoded)
+      const user=await userModel.userModel.findById(decoded.userid)
+      // console.log(user)
+      if (!user) {
+        return res.status(404).json({ message: "User not found" });
+      }
+      req.user = user
       next()
     } catch (error) {
       return res.status(401).json({ message: "Invalid token" });
